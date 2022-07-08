8aa79421285178200f3421fd1717339601a39af9
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-07-08 10:21:04 +0700

    MD homework

---
+++ Module 2/Access modifier/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Student test1 = new Student();
+
+        test1.setName("John Cena");
+        test1.setClasses("C05");
+
+        System.out.println(test1);
+    }
+}
